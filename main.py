1752b265ef9b5b2c80e9ca5c7aea5a210180bfc2
lpaes <lpaes@connexi.com>
2023-07-20 11:08:28 +0100

    inserting the base model | 1_59 min

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainigPipeline
+from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainigPipeline
 
-logger.info('Start LOG custom')
+import subprocess
 
+logger.info('>>>>>>>> Start Model Pipeline <<<<<<<')
 
+# STEP 01
 STAGE_NAME = 'Data ingestion stage'
 try:
     logger.info(f'>>>>>>>>>> {STAGE_NAME} started <<<<<<<<<<')
@@ -13,4 +16,16 @@ try:
     logger.info(f'>>>>>>>>>> {STAGE_NAME} complete <<<<<<<<<<\n\nx===========x')
 except Exception as e:
     logger.exception(e)
+    raise e
+
+# STEP 02
+STAGE_NAME = 'PREPARE BASE MODEL'
+try:
+    logger.info(15 * '*')
+    logger.info(f'>>>>>>>> stage {STAGE_NAME} started <<<<<<<<')
+    obj = PrepareBaseModelTrainigPipeline()
+    obj.main()
+    logger.info(f'>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<\n\nx===========x')
+except Exception as e:
+    logger.info(f'ERROR >>> {e}')
     raise e
---
+++ src/cnnClassifier/pipeline/stage_02_prepare_base_model.py
@@ -0,0 +1,32 @@
+from cnnClassifier.config.configurations import ConfigurationManager
+from cnnClassifier.components.prepare_base_model import PrepareBaseModel
+
+from cnnClassifier import logger
+
+
+
+STAGE_NAME = 'PREPARE BASE MODEL'
+
+class PrepareBaseModelTrainigPipeline:
+    def __init__(self) -> None:
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        prepare_base_model_config = config.get_prepare_base_model_config()
+        prepare_base_model = PrepareBaseModel(config = prepare_base_model_config)
+        prepare_base_model.get_base_model()
+        prepare_base_model.update_base_model()
+
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(15 * '*')
+        logger.info(f'>>>>>>>> stage {STAGE_NAME} started <<<<<<<<')
+        obj = PrepareBaseModelTrainigPipeline()
+        obj.main()
+        logger.info(f'>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<\n\nx===========x')
+    except Exception as e:
+        logger.info(f'ERROR >>> {e}')
+        raise e
